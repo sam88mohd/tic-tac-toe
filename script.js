a6d677539e5b12f352bbd28eb1216e780f3be7c6
samsudin mohamad <sam88mohd@gmail.com>
2022-03-09 16:25:14 +0800

    add write function in displayController module

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-const outerBoxElement = document.querySelector(".outer-box");
+// displayController module
+const displayController = (function () {
+  // get outer-box element
+  const outerBoxElement = document.querySelector(".outer-box");
 
-function makeBox() {
-  outerBoxElement.style.setProperty("--grid-rows", 3);
-  outerBoxElement.style.setProperty("--grid-cols", 3);
+  // create new 3 x 3 board
+  const newBoard = () => {
+    outerBoxElement.style.setProperty("--grid-rows", 3);
+    outerBoxElement.style.setProperty("--grid-cols", 3);
 
-  for (let i = 0; i < 9; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("inner-box");
-    outerBoxElement.appendChild(cell);
-  }
-}
+    for (let i = 0; i < 9; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("inner-box");
+      cell.setAttribute("data-id", i);
+      outerBoxElement.appendChild(cell);
+    }
+  };
 
-makeBox();
+  // write marker to board
+  const write = (marker) => {
+    outerBoxElement.addEventListener("click", (e) => {
+      e.target.textContent = marker;
+    });
+  };
+
+  return { newBoard, write };
+})();
+
+// Gameboard object
+const GameBoard = (() => {
+  let array = [];
+})();
+
+// Player object
+const Player = (marker) => {
+  const getMarker = () => marker;
+  return { getMarker };
+};
+
+displayController.newBoard();
+displayController.write("X");
+
+const player1 = Player("X");
+const player2 = Player("0");
